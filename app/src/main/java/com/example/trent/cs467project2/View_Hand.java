70ccba6f820210534bf68ae7184141fc4ac1889b
Trent <trweatherman1@catamount.wcu.edu>
2015-03-17 18:57:03 -0400

    updated winlist

--- app/src/main/java/com/example/trent/cs467project2/View_Hand.java
+++ app/src/main/java/com/example/trent/cs467project2/View_Hand.java
@@ -1,18 +1,28 @@
 package com.example.trent.cs467project2;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
-public class View_Hand extends Activity {
+public class View_Hand extends ListActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view__hand);
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        //setContentView(R.layout.activity_view__hand);
+        String[] cards = new String[] { "250 Straight Flush", "100 Four of a Kind", "75 Full House",
+                "50 Straight", "25 Three of a Kind", "10 Two Pair", "5 Pair" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, cards);
+        setListAdapter(adapter);
+
+
+
     }
 
 
