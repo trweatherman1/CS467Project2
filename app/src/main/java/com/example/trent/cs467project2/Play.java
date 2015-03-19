661a8769a2a0fc8e0424061665f555375fb2f83b
Cody <clpero1@catamount.wcu.edu>
2015-03-18 22:45:20 -0400

    Updated play screen

--- app/src/main/java/com/example/trent/cs467project2/Play.java
+++ app/src/main/java/com/example/trent/cs467project2/Play.java
@@ -1,18 +1,26 @@
 package com.example.trent.cs467project2;
 
-import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class Play extends Activity {
+public class Play extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -36,5 +44,21 @@ public class Play extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_play_small_screen, container, false);
+            return rootView;
+        }
     }
 }
